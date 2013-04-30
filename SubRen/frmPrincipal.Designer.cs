681e2115560775e5fa31aed0b0fe746e902b21f8
Marcelo Pereira Ramos Rodrigues <marceloprr@gmail.com>
2013-04-29 23:32:00 -0300

    Criacao da Solution

---
+++ SubRen/frmPrincipal.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SubRen
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCaminhoVideos = new System.Windows.Forms.Label();
+            this.txtVideo = new System.Windows.Forms.TextBox();
+            this.btnSelecionarVideo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLegendas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkMesmoVideo = new System.Windows.Forms.CheckBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnProcessaVideo = new System.Windows.Forms.Button();
+            this.fbdSelecionarDiretorios = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lblCaminhoVideos
+            // 
+            this.lblCaminhoVideos.AutoSize = true;
+            this.lblCaminhoVideos.Location = new System.Drawing.Point(13, 13);
+            this.lblCaminhoVideos.Name = "lblCaminhoVideos";
+            this.lblCaminhoVideos.Size = new System.Drawing.Size(70, 13);
+            this.lblCaminhoVideos.TabIndex = 0;
+            this.lblCaminhoVideos.Text = "Local Vídeos";
+            // 
+            // txtVideo
+            // 
+            this.txtVideo.Location = new System.Drawing.Point(102, 10);
+            this.txtVideo.Name = "txtVideo";
+            this.txtVideo.Size = new System.Drawing.Size(431, 20);
+            this.txtVideo.TabIndex = 1;
+            this.txtVideo.TextChanged += new System.EventHandler(this.txtVideo_TextChanged);
+            // 
+            // btnSelecionarVideo
+            // 
+            this.btnSelecionarVideo.Location = new System.Drawing.Point(539, 8);
+            this.btnSelecionarVideo.Name = "btnSelecionarVideo";
+            this.btnSelecionarVideo.Size = new System.Drawing.Size(75, 23);
+            this.btnSelecionarVideo.TabIndex = 2;
+            this.btnSelecionarVideo.Text = "&Selecionar";
+            this.btnSelecionarVideo.UseVisualStyleBackColor = true;
+            this.btnSelecionarVideo.Click += new System.EventHandler(this.btnSelecionarVideo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Se&lecionar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtLegendas
+            // 
+            this.txtLegendas.Location = new System.Drawing.Point(102, 44);
+            this.txtLegendas.Name = "txtLegendas";
+            this.txtLegendas.Size = new System.Drawing.Size(431, 20);
+            this.txtLegendas.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Local Legendas";
+            // 
+            // chkMesmoVideo
+            // 
+            this.chkMesmoVideo.AutoSize = true;
+            this.chkMesmoVideo.Checked = true;
+            this.chkMesmoVideo.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkMesmoVideo.Location = new System.Drawing.Point(621, 46);
+            this.chkMesmoVideo.Name = "chkMesmoVideo";
+            this.chkMesmoVideo.Size = new System.Drawing.Size(115, 17);
+            this.chkMesmoVideo.TabIndex = 6;
+            this.chkMesmoVideo.Text = "&Mesmo dos Videos";
+            this.chkMesmoVideo.UseVisualStyleBackColor = true;
+            this.chkMesmoVideo.CheckedChanged += new System.EventHandler(this.chkMesmoVideo_CheckedChanged);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(13, 73);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(713, 335);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // btnProcessaVideo
+            // 
+            this.btnProcessaVideo.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnProcessaVideo.ForeColor = System.Drawing.Color.LightGreen;
+            this.btnProcessaVideo.Location = new System.Drawing.Point(621, 8);
+            this.btnProcessaVideo.Name = "btnProcessaVideo";
+            this.btnProcessaVideo.Size = new System.Drawing.Size(105, 23);
+            this.btnProcessaVideo.TabIndex = 8;
+            this.btnProcessaVideo.Text = "&Iniciar";
+            this.btnProcessaVideo.UseVisualStyleBackColor = false;
+            this.btnProcessaVideo.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // fbdSelecionarDiretorios
+            // 
+            this.fbdSelecionarDiretorios.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 420);
+            this.Controls.Add(this.btnProcessaVideo);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.chkMesmoVideo);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLegendas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSelecionarVideo);
+            this.Controls.Add(this.txtVideo);
+            this.Controls.Add(this.lblCaminhoVideos);
+            this.Name = "frmPrincipal";
+            this.Text = "SubRen - Subtitle Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCaminhoVideos;
+        private System.Windows.Forms.TextBox txtVideo;
+        private System.Windows.Forms.Button btnSelecionarVideo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLegendas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkMesmoVideo;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnProcessaVideo;
+        private System.Windows.Forms.FolderBrowserDialog fbdSelecionarDiretorios;
+    }
+}
+
---
+++ SubRen/frmPrincipal.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SubRen
+{
+
+    public partial class frmPrincipal : Form
+    {
+        private String CaminhoVideos { get; set; }
+        private String CaminhoLegendas { get; set; }
+
+        private string[] extensoesVideos = new string[] { ".avi", ".mp4", ".mkv" };
+        private string[] extensoesLegendas = new string[2] { ".srt", ".sub" };
+        private string[] chavesSeason = new string[4] { "SSSEEE", "SEE", "SxEE", "SSxEE" };
+
+        private readonly string ARQUIVO_WILD_CARD = "*";
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+#if (DEBUG)
+            SetCaminhoVideos(@"D:\MarceloP\Videos\");
+            chkMesmoVideo.Checked = false;
+            SetCaminhoLegendas(@"D:\MarceloP\Legendas");
+#endif
+            ProcessaCheckMesmoVideo();
+        }
+
+        private void btnSelecionarVideo_Click(object sender, EventArgs e)
+        {
+            fbdSelecionarDiretorios.SelectedPath = txtVideo.Text;
+            if (fbdSelecionarDiretorios.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SetCaminhoVideos(fbdSelecionarDiretorios.SelectedPath);
+            }
+        }
+
+        private void SetCaminhoVideos(string caminho)
+        {
+            CaminhoVideos = caminho;
+            txtVideo.Text = caminho;
+            if (chkMesmoVideo.Checked)
+            {
+                SetCaminhoLegendas(CaminhoVideos);
+            }
+        }
+
+        private void SetCaminhoLegendas(string caminho)
+        {
+            CaminhoLegendas = caminho;
+            txtLegendas.Text = caminho;
+        }
+
+        private void chkMesmoVideo_CheckedChanged(object sender, EventArgs e)
+        {
+            ProcessaCheckMesmoVideo();
+        }
+
+        private void ProcessaCheckMesmoVideo()
+        {
+            if (chkMesmoVideo.Checked)
+            {
+                txtLegendas.Enabled = false;
+                SetCaminhoLegendas(txtVideo.Text);
+            }
+            else
+            {
+                txtLegendas.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CaminhoVideos = txtVideo.Text;
+            CaminhoLegendas = txtLegendas.Text;
+            if (validaParametros())
+            {
+                ProcessaArquivos();
+            }
+        }
+
+        private bool validaParametros()
+        {
+            return consisteCaminho(CaminhoVideos) && consisteCaminho(CaminhoLegendas);
+        }
+
+        private bool consisteCaminho(string caminho)
+        {
+            if (String.IsNullOrWhiteSpace(caminho) || !Directory.Exists(caminho))
+            {
+                MessageBox.Show("O(s) diretório(s) selecionado(s) deve(m) ser válido(s).\nFavor verificar o preenchimento", "Erro no diretório", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void ProcessaArquivos()
+        {
+            txtResultado.Clear();
+            ProcessaDiretorio(CaminhoVideos);
+        }
+
+        private void ProcessaDiretorio(string caminho)
+        {
+            var listaVideos = new List<string>();
+            foreach (var extensao in extensoesVideos)
+            {
+                listaVideos.AddRange(Directory.GetFiles(caminho, insereWildCard(extensao)).ToList());
+            }
+            txtResultado.AppendText(String.Concat("Processando Diretório: ", caminho, System.Environment.NewLine));
+            foreach (var arquivoCompleto in listaVideos)
+            {
+                var arquivo = new FileInfo(arquivoCompleto);
+                txtResultado.AppendText(String.Format("\t {0} ... ", arquivo.Name));
+                if (ProcuraLegendas(arquivo.Name, caminho))
+                {
+                    txtResultado.AppendText("Processado OK!");
+                }
+                else
+                {
+                    txtResultado.AppendText("Legenda não encontrada!");
+                }
+                txtResultado.AppendText(System.Environment.NewLine);
+            }
+        }
+
+        private string insereWildCard(string extensao)
+        {
+            return String.Concat(ARQUIVO_WILD_CARD, extensao);
+        }
+
+        private bool ProcuraLegendas(string nomeArquivoVideo, string caminhoDestino)
+        {
+            foreach (var extensao in extensoesLegendas)
+            {
+                if (TestaArquivoEMove(caminhoDestino, Path.ChangeExtension(nomeArquivoVideo, extensao)))
+                {
+                    return true;
+                }
+                else if (TestaArquivoEMove(caminhoDestino, GeraPossibilidadeDeArquivos(nomeArquivoVideo, extensao), nomeArquivoVideo))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private string GeraPossibilidadeDeArquivos(string nomeArquivoVideo, string extensao)
+        {
+            //var ret = new List<string>();
+            var ret = String.Empty;
+            var reg = String.Empty;
+            var mascara = "S0{0}E{1}{2}";
+            var idxSeason = 0;
+            var idxEpisodio = 0;
+            foreach (var chaveSeason in chavesSeason)
+            {
+                switch (chaveSeason)
+                {
+                    case "SSSEEE":
+                        {
+                            reg = @"S\d{2}E\d{2}";
+                            idxSeason = 2;
+                            idxEpisodio = 4;
+                        } break;
+                    case "SEE":
+                        {
+                            reg = @" \d{3} ";
+                            idxSeason = 1;
+                            idxEpisodio = 2;
+                        } break;
+                    case "SxEE":
+                        {
+                            reg = @" - \d{1}[x]\d{2}";
+                            idxSeason = 3;
+                            idxEpisodio = 5;
+                        } break;
+                    case "SSxEE":
+                        {
+                            reg = @" - \d{2}[x]\d{2}";
+                            idxSeason = 4;
+                            idxEpisodio = 6;
+                        } break;
+                }
+
+                Regex rx = new Regex(reg, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                var seasonEEpisodio = rx.Match(nomeArquivoVideo).Value;
+                if (!String.IsNullOrWhiteSpace(seasonEEpisodio))
+                {
+                    var posSeason = nomeArquivoVideo.IndexOf(seasonEEpisodio);
+                    var nomeSerie = nomeArquivoVideo.Substring(0, posSeason);
+                    nomeSerie = nomeSerie.Replace(" ", ARQUIVO_WILD_CARD);
+                    seasonEEpisodio = String.Format(mascara, seasonEEpisodio[idxSeason], seasonEEpisodio[idxEpisodio++], seasonEEpisodio[idxEpisodio]);
+                    var arquivoLegenda = String.Concat(nomeSerie, ARQUIVO_WILD_CARD, seasonEEpisodio, ARQUIVO_WILD_CARD, extensao);
+                    var listArquivos = Directory.GetFiles(CaminhoLegendas, arquivoLegenda);
+                    if (listArquivos.Count() == 1)
+                    {
+                        ret = listArquivos.First();
+                        break;
+                    }
+                }
+            }
+            return ret;
+        }
+
+        private bool TestaArquivoEMove(string caminhoDestino, string arquivoLegenda)
+        {
+            return TestaArquivoEMove(caminhoDestino, arquivoLegenda, Path.GetFileName(arquivoLegenda));
+        }
+
+        private bool TestaArquivoEMove(string caminhoDestino, string arquivoLegenda, string nomeArquivoVideo)
+        {
+            var arquivoASerCopiado = Path.Combine(CaminhoLegendas, arquivoLegenda);
+            if (File.Exists(arquivoASerCopiado))
+            {
+                var arquivoDestino = Path.Combine(caminhoDestino, Path.ChangeExtension(nomeArquivoVideo, Path.GetExtension(arquivoLegenda)));
+                if (!arquivoASerCopiado.Equals(arquivoDestino))
+                {
+                    if (File.Exists(arquivoDestino))
+                    {
+                        File.Delete(arquivoDestino);
+                    }
+                    File.Move(arquivoASerCopiado, arquivoDestino);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void txtVideo_TextChanged(object sender, EventArgs e)
+        {
+            if (chkMesmoVideo.Checked)
+            {
+                txtLegendas.Text = txtVideo.Text;
+            }
+        }
+    }
+}
